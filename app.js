b4bc80e1828ede96a572be7e1b1a531df36a62f2
Rafelesueg <raffaelenapoli91@gmail.com>
2023-11-13 17:48:57 +0100

    Update

--- app.js
+++ app.js
@@ -21,90 +21,53 @@ resetShopping.addEventListener('click', ()=>{
 })
 
 let products = [
-    {
-        id: 1,
-        name: 'Rame',
-        price: 5,
-        group: 'metalli'
-    },
-    {
-        id: 2,
-        name: 'Ferro',
-        price: 5,
-        group: 'metalli'
-    },
-    {
-        id: 3,
-        name: 'Stagno',
-        price: 5,
-        group: 'metalli'
-    },
-    {
-        id: 4,
-        name: 'Pietra',
-        price: 5,
-        group: 'metalli'
-    },
-    {
-        id: 5,
-        name: 'Sabbia',
-        price: 5,
-        group: 'metalli'
-    },
-    {
-        id: 6,
-        name: 'Grafite',
-        price: 5,
-        group: 'metalli'
-    },
-    {
-        id: 7,
-        name: 'Item1',
-        price: 5,
-        group: 'altro'
-    },
-    {
-        id: 8,
-        name: 'Item2',
-        price: 5,
-        group: 'altro'
-    },
-    {
-        id: 9,
-        name: 'Item3',
-        price: 5,
-        group: 'altro'
-    },
-    {
-        id: 10,
-        name: 'Item4',
-        price: 5,
-        group: 'altro'
-    },
-    {
-        id: 11,
-        name: 'Walkman',
-        price: 5,
-        group: 'metaldetector'
-    },
-    {
-        id: 12,
-        name: 'Chiave Inglese',
-        price: 5,
-        group: 'metaldetector'
-    },
-    {
-        id: 13,
-        name: 'Bullone',
-        price: 5,
-        group: 'metaldetector'
-    },
-    {
-        id: 14,
-        name: 'Pistola',
-        price: 5,
-        group: 'metaldetector'
-    },
+    { id: 1, name: 'Rame', price: 5, group: 'metalli' },
+    { id: 2, name: 'Ferro', price: 5, group: 'metalli' },
+    { id: 3, name: 'Stagno', price: 5, group: 'metalli' },
+    { id: 4, name: 'Pietra', price: 5, group: 'metalli' },
+    { id: 5, name: 'Sabbia', price: 5, group: 'metalli' },
+    { id: 6, name: 'Grafite', price: 5, group: 'metalli' },
+    { id: 7, name: 'Radio', price: 5, group: 'altro' },
+    { id: 8, name: 'AudioCassetta', price: 5, group: 'altro' },
+    { id: 9, name: 'Polaroid', price: 5, group: 'altro' },
+    { id: 10, name: 'Cercapersone', price: 70, group: 'altro' },
+    { id: 11, name: 'Walkman', price: 120, group: 'metaldetector' },
+    { id: 12, name: 'Chiave Inglese', price: 5, group: 'metaldetector' },
+    { id: 13, name: 'Bullone', price: 5, group: 'metaldetector' },
+    { id: 14, name: 'Accendino', price: 2, group: 'metaldetector' },
+    { id: 15, name: 'Annaffiatoio', price: 4, group: 'altro' },
+    { id: 16, name: 'Cassetta', price: 3, group: 'altro' },
+    { id: 17, name: 'Batteria', price: 5, group: 'altro' },
+    { id: 18, name: 'Benzina ($1 al litro)', price: 5, group: 'altro' },
+    { id: 19, name: 'Bullone', price: 4, group: 'altro' },
+    { id: 20, name: 'Cacciavite', price: 6, group: 'altro' },
+    { id: 21, name: 'Camera', price: 40, group: 'altro' },
+    { id: 22, name: 'Cartella', price: 1, group: 'altro' },
+    { id: 23, name: 'Cartine', price: 0.5, group: 'altro' },
+    { id: 24, name: 'Cera', price: 2, group: 'altro' },
+    { id: 25, name: 'Chiavi-Manette', price: 10, group: 'altro' },
+    { id: 26, name: 'Fascetta', price: 5, group: 'altro' },
+    { id: 27, name: 'Foglio', price: 0.10, group: 'altro' },
+    { id: 28, name: 'Forbici', price: 2, group: 'altro' },
+    { id: 29, name: 'Gettone', price: 0.10, group: 'altro' },
+    { id: 30, name: 'Grimaldelli auto($1-$3)', price: 1, group: 'altro' },
+    { id: 31, name: 'Grimaldello comune($1-$3)', price: 1, group: 'altro' },
+    { id: 32, name: 'Manette', price: 25, group: 'altro' },
+    { id: 33, name: 'Metal Detector', price: 755, group: 'altro' },
+    { id: 34, name: 'Penna', price: 1, group: 'altro' },
+    { id: 35, name: 'Portachiavi', price: 1, group: 'altro' },
+    { id: 36, name: 'Portafoglio', price: 20, group: 'altro' },
+    { id: 37, name: 'Radio', price: 120, group: 'altro' },
+    { id: 38, name: 'Rottami', price: 1, group: 'altro' },
+    { id: 39, name: 'Sacco', price: 3, group: 'altro' },
+    { id: 40, name: 'Spellacavi', price: 35, group: 'altro' },
+    { id: 41, name: 'Stetoscopio', price: 30, group: 'altro' },
+    { id: 42, name: 'Supporto Tunnel', price: 60, group: 'altro' },
+    { id: 43, name: 'Tappo', price: 2.5, group: 'altro' },
+    { id: 44, name: 'TNI', price: 500, group: 'altro' },
+    { id: 45, name: 'Torcia', price: 120, group: 'altro' },
+    { id: 46, name: 'Spugna', price: 5, group: 'altro' },
+    { id: 47, name: 'USB', price: 100, group: 'altro' }
 ];
 
 let listCards  = [];
@@ -141,15 +104,12 @@ function filterByGroup() {
     let groupDropdown = document.getElementById('groupDropdown');
     let selectedGroup = groupDropdown.value;
 
-    // Mostra tutti gli elementi
     Array.from(list.children).forEach((item) => {
         item.style.display = 'block';
     });
 
-    // Nascondi gli elementi che non appartengono al gruppo selezionato
     if (selectedGroup !== 'all') {
         Array.from(list.children).forEach((item) => {
-            // Modifica questa riga per ottenere il gruppo dall'elemento corretto
             let itemGroup = item.getAttribute('data-group').toLowerCase();
 
             if (itemGroup !== selectedGroup) {
@@ -157,8 +117,6 @@ function filterByGroup() {
             }
         });
     }
-
-    // Ricarica la lista del carrello dopo aver filtrato
     reloadCard();
 }
 
